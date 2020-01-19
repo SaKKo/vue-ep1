f33031311072b6315f2a436e11babeed91ff7026
sakko <saklism@gmail.com>
2020-01-19 16:12:50 +0700

    added api call

---
+++ pages/books.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li v-for="book in books">
+        {{ book.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      books: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3003/books')
+      .then((response) => {
+        // handle success
+        this.books = response.data
+        console.log(response)
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error)
+      })
+      .finally(() => {
+        // always executed
+      })
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
